d955f60a1a877224afa1b7b256f31c8ec68f005f
Edmond <edpodlegaev@yandex.ru>
2023-12-11 13:12:35 +0300

    Add gesture

--- SparrowCode_FifthTask/ContentView.swift
+++ SparrowCode_FifthTask/ContentView.swift
@@ -10,14 +10,28 @@ import SwiftUI
 var colors: [Color] = [Color.white, Color.pink, Color.yellow, Color.black]
 
 struct ContentView: View {
+    @State var rectanglePosition: CGSize = .zero
+    
     var body: some View {
-        VStack(spacing: 0) {
-            ForEach(colors, id: \.self) { color in
-                Rectangle()
-                    .foregroundStyle(color)
+        ZStack {
+            VStack(spacing: 0) {
+                ForEach(colors, id: \.self) { color in
+                    Rectangle()
+                        .foregroundStyle(color)
+                }
             }
+            .ignoresSafeArea()
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundStyle(Color.green)
+                .frame(width: 125, height: 125)
+                .offset(rectanglePosition)
+                .gesture(DragGesture()
+                    .onChanged({ gesture in
+                        rectanglePosition = gesture.translation
+                    })
+                )
         }
-        .ignoresSafeArea()
     }
 }
 
