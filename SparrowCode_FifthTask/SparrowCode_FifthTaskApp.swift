9e0e2fd01e6249add4244de4c00207974e92c0b5
Edmond <edpodlegaev@yandex.ru>
2023-12-11 11:23:08 +0300

    Initial Commit

---
+++ SparrowCode_FifthTask/SparrowCode_FifthTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCode_FifthTaskApp.swift
+//  SparrowCode_FifthTask
+//
+//  Created by Edmond Podlegaev on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCode_FifthTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
